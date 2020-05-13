904100da1c46b52227f7359078d548e534411d07
Lalu Moh Adam Kurniawan <64571579+adamkurniawans@users.noreply.github.com>
2020-05-13 10:05:06 +0800

    Create Jobsheet 1 C.c
    Membuat array satu dimensi yang menampilkan NIM Mahasiswa

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+using namespace std;
+
+int main(){
+int myarray[8] = {12, 63 , 32, 78, 4 , 90 , 65, 78};
+	
+printf("Nama     : LALU MOH. ADAM KURNIAWAN\n");
+printf("NIM      : F1B019078\n");
+printf("Kelompok : 16\n");
+	
+printf("\nData ke-1 = %d",myarray[0]);
+printf("\n");
+printf("Data ke-2 = %d",myarray[1]);
+printf("\n");
+printf("Data ke-3 = %d",myarray[2]);
+printf("\n");
+printf("Data ke-4 = %d",myarray[3]);
+printf("\n");
+printf("Data ke-5 = %d",myarray[4]);
+printf("\n");
+printf("Data ke-6 = %d",myarray[5]);
+printf("\n");
+printf("Data ke-7 = %d",myarray[6]);
+printf("\n");
+printf("Data ke-8 = %d",myarray[7]);
+		
+}
