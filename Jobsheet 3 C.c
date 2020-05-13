ff3bb25ef82c9d6f355fbbb428b8d4b8a2b5aba5
Lalu Moh Adam Kurniawan <64571579+adamkurniawans@users.noreply.github.com>
2020-05-13 10:51:50 +0800

    Create Jobsheet 3 C.c
    Membuat program penjumlahan matriks dua dimensi dengan menggunakan NIM dua orang mahasiswa

---
+++ Jobsheet 3 C.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <conio.h>
+
+main(){
+	printf("Nama     : LALU MOH. ADAM KURNIAWAN\n");
+	printf("NIM      : F1B019078\n");
+	printf("Kelompok : 16\n\n");
+	
+	int matrix1[2][2], matrix2[2][2] ,i,j;
+	printf("MATRIX 1 \n");
+	for(i=0; i<2; i++){
+		for(j=0; j<2; j++){
+			printf("Elemen [%d][%d] : ",i,j);
+			scanf("%d",&matrix1[i][j]);
+		}
+	}
+	printf("\nMATRIX 2 \n");
+	for(i=0; i<2; i++){
+		for(j=0; j<2; j++){
+			printf("Elemen [%d][%d] : ",i,j);
+			scanf("%d",&matrix2[i][j]);
+		}
+	}
+	printf("\nHASIL PENJUMLAHAN\n\n");
+	for(i=0; i<2; i++){
+		for(j=0; j<2; j++){
+			printf("%d\t",matrix1[i][j] + matrix2[i][j]);
+			printf(" ");
+		}printf("\n");
+	}
+}
+
