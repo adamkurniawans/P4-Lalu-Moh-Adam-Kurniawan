a0ed4646a25dcc5fc0202cfd365b4afd08af179d
Lalu Moh Adam Kurniawan <64571579+adamkurniawans@users.noreply.github.com>
2020-05-13 10:10:00 +0800

    Create Jobsheet 1 C++.cpp
    Membuat array satu dimensi yang menampilkan NIM Mahasiswa

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int myarray[8] = {12, 63 , 32, 78, 4 , 90 , 65, 78};
+	
+	cout << "Nama     : LALU MOH. ADAM KURNIAWAN" << endl;
+	cout << "NIM      : F1B019078" << endl;
+	cout << "Kelompok : 16" << endl << endl;
+		cout<<"Data ke-1 = "<<myarray[0] << endl;
+		cout<<"Data ke-2 = "<<myarray[1] << endl;
+		cout<<"Data ke-3 = "<<myarray[2] << endl;
+		cout<<"Data ke-4 = "<<myarray[3] << endl;
+		cout<<"Data ke-5 = "<<myarray[4] << endl;
+		cout<<"Data ke-6 = "<<myarray[5] << endl;
+		cout<<"Data ke-7 = "<<myarray[6] << endl;
+		cout<<"Data ke-8 = "<<myarray[7] << endl;
+
+}
