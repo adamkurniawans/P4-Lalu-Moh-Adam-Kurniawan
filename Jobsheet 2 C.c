251427bf7595bf431af9ba39c7f2200025868690
Lalu Moh Adam Kurniawan <64571579+adamkurniawans@users.noreply.github.com>
2020-05-13 10:45:18 +0800

    Create Jobsheet 2 C.c
    Mengisi dan menampilkan elemen array index berdasarkan NIM Mahasiswa

---
+++ Jobsheet 2 C.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(){
+	int x [8];
+	printf("Nama     : LALU MOH. ADAM KURNIAWAN\n");
+	printf("NIM      : F1B019078\n");
+	printf("Kelompok : 16\n\n");
+	
+	printf("Masukkkan nilai 1: ");
+	scanf("%i",&x[0]);
+	printf("Masukkkan nilai 2: ");
+	scanf("%i",&x[1]);
+	printf("Masukkkan nilai 3: ");
+	scanf("%i",&x[2]);
+	printf("Masukkkan nilai 4: ");
+	scanf("%i",&x[3]);
+	printf("Masukkkan nilai 5: ");
+	scanf("%i",&x[4]);
+	printf("Masukkkan nilai 6: ");
+	scanf("%i",&x[5]);
+	printf("Masukkkan nilai 7: ");
+	scanf("%i",&x[6]);
+	printf("Masukkkan nilai 8: ");
+	scanf("%i",&x[7]);
+	printf("\n");
+	
+	printf("Nilai ke-1 = %i\n",x[0]);
+	printf("Nilai ke-2 = %i\n",x[1]);
+	printf("Nilai ke-3 = %i\n",x[2]);
+	printf("Nilai ke-4 = %i\n",x[3]);
+	printf("Nilai ke-5 = %i\n",x[4]);
+	printf("Nilai ke-6 = %i\n",x[5]);
+	printf("Nilai ke-7 = %i\n",x[6]);
+	printf("Nilai ke-8 = %i\n",x[7]);
+	
+	
+}
