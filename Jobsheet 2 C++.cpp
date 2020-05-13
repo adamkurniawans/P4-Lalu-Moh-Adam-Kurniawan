d60d582a36ff126ee84b48de50265f2f2c0ebe05
Lalu Moh Adam Kurniawan <64571579+adamkurniawans@users.noreply.github.com>
2020-05-13 10:49:33 +0800

    Create Jobsheet 2 C++.cpp
    Mengisi dan menampilkan elemen array index berdasarkan NIM

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Nama     : LALU MOH. ADAM KURNIAWAN" << endl;
+	cout << "NIM      : F1B019078" << endl;
+	cout << "Kelompok : 16" << endl << endl;
+	
+	int x [8];
+	
+	cout<<"Masukkan nilai 1: ";
+	cin>>x[0];
+	
+	cout<<"Masukkan nilai 2: ";
+	cin>>x[1];
+	
+	cout<<"Masukkan nilai 3: ";
+	cin>>x[2];
+	
+	cout<<"Masukkan nilai 4: ";
+	cin>>x[3];
+	
+	cout<<"Masukkan nilai 5: ";
+	cin>>x[4];
+	
+	cout<<"Masukkan nilai 6: ";
+	cin>>x[5];
+	
+	cout<<"Masukkan nilai 7: ";
+	cin>>x[6];
+	
+	cout<<"Masukkan nilai 8: ";
+	cin>>x[7];
+	cout<<endl;
+	
+	cout<<"Nilai ke-1 =  " << x[0] << endl;
+	cout<<"Nilai ke-2 =  " << x[1] << endl;
+	cout<<"Nilai ke-3 =  " << x[2] << endl;
+	cout<<"Nilai ke-4 =  " << x[3] << endl;
+	cout<<"Nilai ke-5 =  " << x[4] << endl;
+	cout<<"Nilai ke-6 =  " << x[5] << endl;
+	cout<<"Nilai ke-7 =  " << x[6] << endl;
+	cout<<"Nilai ke-8 =  " << x[7] << endl;
+	system("PAUSE");
+	
+}
